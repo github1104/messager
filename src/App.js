cd085d333a96686edff9882a238b6bafb120c4bf
tam198 <bigteddy198@gmail.com>
2020-08-31 10:46:50 +0700

    tao login form

--- src/App.js
+++ src/App.js
@@ -1,64 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { Button, FormControl, InputLabel, Input,IconButton } from '@material-ui/core';
-import './App.css';
-import Message from './Message';
-import db from './firebase';
-import firebase from 'firebase';
-import FlipMove from 'react-flip-move';
-import SendIcon from '@material-ui/icons/Send';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect
+} from "react-router-dom";
+
+import "./App.css";
+
+import DialogBox from "./DialogBox";
+import Login from "./Login";
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  },
+};
+
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        fakeAuth.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
 
 function App() {
-  const [input, setInput] = useState('');
-  const [messages, setMessages] = useState([]);
-  const [userName, setUserName] = useState('');
-
-  useEffect(() => {
-    db.collection('messages')
-      .orderBy('timestamp', 'desc')
-      .onSnapshot(snapshot => {
-        setMessages(snapshot.docs.map(doc => ({ id: doc.id, message: doc.data() })))
-      })
-      }, [])
-
-    useEffect(() => {
-      setUserName(prompt('Enter your username'))
-    }, [])
-
-    const sendMessage = (event) => {
-      event.preventDefault();
-
-      db.collection('messages').add({
-        message: input,
-        username: userName,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp()
-      })
-      setInput('');
-    }
-
-    return (
+  return (
+    <Router>
       <div className="App">
-        <img src="https://www.logo.wine/a/logo/Facebook_Messenger/Facebook_Messenger-Logo.wine.svg" width="250px"/>
-        <h2>Hi {userName}</h2>
-        <form className="app__form">
-          <FormControl className="app__formControl">
-            <Input className="app__input" placeholder="Enter the message..." value={input} onChange={event => setInput(event.target.value)} />
-            <IconButton className="app__iconButon" disabled={!input} type='submit' variant="contained" color="primary" onClick={sendMessage}>
-              <SendIcon />
-            </IconButton>
-          </FormControl>
-        </form>
-
-        <FlipMove>
-          {
-            messages.map(({id, message}) => (
-              <Message key={id} username={userName} message={message} />
-            ))
-          }
-
-        </FlipMove>
-
+        <Route exact path="/" component={Login} />
+        <Route path="/dialogbox/:nameUser" component={DialogBox} />
       </div>
-    );
-  }
+    </Router>
+  );
+}
 
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { BrowserRouter as Router, Redirect, Link } from "react-router-dom";
+
+import "./Login.css";
+
+export default function Login(props) {
+  const [nameUser, setNameUser] = useState("");
+
+  const ColorButton = withStyles((theme) => ({
+    root: {
+      color: theme.palette.getContrastText("#508ac9"),
+      backgroundColor: "#508ac9",
+      "&:hover": {
+        backgroundColor: "#508ac9",
+      },
+    },
+  }))(Button);
+
+  const login = () => {
+    console.log(nameUser);
+    // return <Redirect to="/dialogbox" />;
+    props.history.push(`/dialogbox/${nameUser}`);
+  };
+
+  return (
+    <Router>
+      <div className="LoginBox">
+        <form className="LoginForm" noValidate autoComplete="off">
+          <h2>Kham Thien Arena</h2>
+          <TextField
+            id="outlined-basic"
+            label="Username"
+            variant="outlined"
+            value={nameUser}
+            onChange={(event) => setNameUser(event.target.value)}
+            style={{ marginBottom: "30px" }}
+          />
+          <ColorButton
+            variant="contained"
+            color="primary"
+            className="customButton"
+            onClick={login}
+          >
+            Login
+          </ColorButton>
+          {/* <Link to="/dialogbox">Users</Link> */}
+        </form>
+      </div>
+    </Router>
+  );
+}
